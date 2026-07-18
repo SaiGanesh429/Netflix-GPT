075bf96ef29f24378f9e3c7a639a5e3c4fa36919
Sai Ganesh <kanigolla.saiganesh@gmail.com>
2026-07-18 20:04:53 +0530

    signIn and signUp pages

--- src/components/Login/SignInForm/SignInForm.jsx
+++ src/components/Login/SignInForm/SignInForm.jsx
@@ -1,27 +1,46 @@
-
+import { useRef, useState } from "react";
+import validateFormFields from "../../../utils/validateFormFields";
 
 const SignInForm = () => {
-    return (
-        <form className="flex flex-col items-center p-4">
-            <input
-                className="m-4 w-64 h-10 text-white bg-gray-500 text-black p-2"
-                type="email"
-                placeholder="Email or mobile number"
-            />
-            <input
-                className="m-4 w-64 h-10 text-white bg-gray-500 text-black p-2"
-                type="password"
-                placeholder="Password"
-            />
-            <button
-                className="m-4 w-64 h-10 bg-red-600 text-white rounded border-solid border-2"
-                type="submit"
-            >
-                Login
-            </button>
-        </form>
-    )
-}
+  const [formErrors, setFormErrors] = useState(null);
+  const email = useRef(null);
+  const password = useRef(null);
+
+
+  const validateSignInForm = () => {
+    const signInFormErrors = validateFormFields(email.current.value, password.current.value);
+    console.log(signInFormErrors);
+    setFormErrors(signInFormErrors);
+
+  };
+
+  return (
+    <form
+      onSubmit={(event) => event.preventDefault()}
+      className="flex flex-col items-center p-4"
+    >
+      <input
+        ref={email}
+        className="m-4 w-64 h-10 text-white bg-gray-500 text-black p-2"
+        type="email"
+        placeholder="Email or mobile number"
+      />
+      <input
+        ref={password}
+        className="m-4 w-64 h-10 text-white bg-gray-500 text-black p-2"
+        type="password"
+        placeholder="Password"
+      />
+      <label className="text-red-700 text-sm font-semibold">{formErrors}</label>
+      <button
+        className="m-4 w-64 h-10 bg-red-600 text-white rounded border-solid border-2"
+        type="submit"
+        onClick={validateSignInForm}
+      >
+        Login
+      </button>
+    </form>
+  );
+};
 
 export default SignInForm;
-
--- src/components/Login/SignUpForm/SignUpForm.jsx
+++ src/components/Login/SignUpForm/SignUpForm.jsx
@@ -1,25 +1,41 @@
+import { useRef, useState } from "react";
+import validateFormFields from "../../../utils/validateFormFields";
+
 const SignUpForm = () => {
+
+  const [formErrors, setFormErrors] = useState(null);
+  const email = useRef(null);
+  const password = useRef(null);
+
+  const handleSignUpForm = () => {
+    const signUpFormErrors = validateFormFields(email.current.value, password.current.value)
+    setFormErrors(signUpFormErrors);
+  }
   return (
     <div>
-      <form className="flex flex-col items-center p-4">
+      <form onSubmit={(e) => e.preventDefault()} className="flex flex-col items-center p-4">
         <input
+
           className="m-4 w-64 h-10 text-white bg-gray-500 text-black p-2"
           type="text"
           placeholder="Name"
         />
         <input
+          ref={email}
           className="m-4 w-64 h-10 text-white bg-gray-500 text-black p-2"
           type="email"
           placeholder="Email or mobile number"
         />
         <input
+          ref={password}
           className="m-4 w-64 h-10 text-white bg-gray-500 text-black p-2"
           type="password"
           placeholder="Password"
         />
+        <label className="text-red-700 text-sm font-semibold">{formErrors}</label>
         <button
           className="m-4 w-64 h-10 bg-red-600 text-white rounded border-solid border-2"
-          type="submit"
+          type="submit" onClick={handleSignUpForm}
         >
           Sign UP
         </button>
